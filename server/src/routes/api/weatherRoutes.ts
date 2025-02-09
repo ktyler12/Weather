8b3adeb5f23bb381f6971b33b1de76b5c1f65a00
ktyler12 <101907859+ktyler12@users.noreply.github.com>
2025-02-09 13:46:12 -0500

    Added starter code and post route

---
+++ server/src/routes/api/weatherRoutes.ts
@@ -0,0 +1,26 @@
+import { Router, type Request, type Response } from 'express';
+const router = Router();
+
+import HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
+
+// TODO: POST Request with city name to retrieve weather data
+router.post('/', (req: Request, res: Response) => {
+
+  // TODO: GET weather data from city name
+  WeatherService.getWeatherForCity(req.body.city)
+    .then(data => {
+      console.log(data);
+      HistoryService.addCity(req.body.city)
+
+      res.json(data)
+    })
+  // TODO: save city to search history
+})
+// TODO: GET search history
+router.get('/history', async (req: Request, res: Response) => { });
+
+// * BONUS TODO: DELETE city from search history
+router.delete('/history/:id', async (req: Request, res: Response) => { });
+
+export default router;
